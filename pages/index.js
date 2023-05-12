3774c9542220dff5da0705500508925ad85424c3
harekyon <harekyon@gmail.com>
2023-05-12 20:55:38 +0900

    スクロールで見え隠れするアドレスバーをそうさせないように

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      {/* <div className="wrap"> */}
+      <div className="section1"></div>
+      <div className="section2"></div>
+      <div className="section3"></div>
+      <div className="section4"></div>
+      {/* </div> */}
+    </>
+  );
+}
